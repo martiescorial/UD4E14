e312613130bb8db3fbd68c87872a074c51c1b6a6
Martina <martinaescorial9@gmail.com>
2021-06-19 14:56:11 +0200

    Add project files.

---
+++ UD4E14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UD4E14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String texto = "Introduce area del circulo";
+            
+            double radio = Convert.ToDouble(texto);
+
+            //Formula area circulo, usamos algunos de los metodos de Math
+            double area = Math.PI * Math.Pow(radio, 2);
+
+            Console.WriteLine("El area del circulo es " + area);
+        }
+    }
+}
